ff1d386548f53ff97def9d1ba45aa55cee9f5b22
Anurag Rawat <anurag.rawat0201@gmail.com>
2023-03-21 19:49:16 +0000

    add spinner; add tabs

--- apps/mobile/src/app/app.module.ts
+++ apps/mobile/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
